cd2a8a280f0a3f0829052e97c1f9728e74563e95
Daniel Trujillo <danielfetrujillo@gmail.com>
2023-09-01 19:32:32 -0500

    sends and receives LN payments

--- App.js
+++ App.js
@@ -8,6 +8,8 @@ import {
   EnvironmentType,
   connect,
   nodeInfo,
+  receivePayment,
+  sendPayment,
 } from '@breeztech/react-native-breez-sdk';
 
 import {BREEZ_API_KEY, BREEZ_INVITE_CODE, MNEMONIC_WORDS} from '@env';
@@ -63,11 +65,32 @@ const App = () => {
     }
   };
 
+  const receiveLNPayment = async () => {
+    try {
+      const invoice = await receivePayment(100, 'Invoice for 100 sats');
+      console.log(invoice);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const sendLNPayment = async () => {
+    const bolt11 =
+      'lnbc500n1pj09qdfpp52fg7hjf327j65ukvfa8fqwaas9m0ggx5zt3nqe5emy3hs77qd78qdqqcqzzsxqyz5vqsp5l303att3urq929l9tzm0heknhrfnegks6pkkv89y8ytv59gtn4gs9qyyssq54qakegz9va5dfjv7gh5zvyqsu7zv56jvn35ps3mxp45mxpu7eek3462qwjuleejqpwxu0tx0s3e8pf2la0vm60nplh46wfxnt42srcqrky2th';
+    try {
+      const payment = await sendPayment(bolt11);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <View>
-      <Text>Lightning {balance.lightning ?? '0'}</Text>
+      <Text>Lightning {balance.lightning ?? '0'} msat</Text>
       <Text>BTC {balance.btc ?? '0'}</Text>
       <Button onPress={checkBalance} title="Check Balance" />
+      <Button onPress={receiveLNPayment} title="Receive LN Payment" />
+      <Button onPress={sendLNPayment} title="Send LN Payment" />
     </View>
   );
 };
